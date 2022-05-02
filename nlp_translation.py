a6d8036deb4fda9f6dc98718f28ffe60556af0c8
804459116@qq.com <804459116@qq.com>
2022-05-02 20:30:43 +0800

    nlp_translation

--- nlp_translation.py
+++ nlp_translation.py
@@ -161,21 +161,21 @@ def train_step(inp,targ,enc_hidden):
     optimizer.apply_gradients(zip(gradients,variables))
     return batch_loss
 
-# EPOCHS=2
-#
-# for epoch in range(EPOCHS):
-#     start=time.time()
-#     enc_hidden=encoder.initialize_hidden_state()
-#     total_loss=0
-#     for (batch,(inp,targ)) in enumerate(dataset.take(steps_per_epoch)):
-#         batch_loss=train_step(inp,targ,enc_hidden)
-#         total_loss+=batch_loss
-#         if batch%5==0:
-#             print("epoch {} Batch {} loss{}".format(epoch+1,batch,batch_loss.numpy()))
-#     if (epoch+1)%2==0:
-#         checkpoint.save(file_prefix=checkpoint_prefix)
-#     print("Epoch {} loss{}".format(epoch+1,total_loss/steps_per_epoch))
-#     print("time taken for 1 epoch{}".format(time.time()-start))
+EPOCHS=30
+
+for epoch in range(EPOCHS):
+    start=time.time()
+    enc_hidden=encoder.initialize_hidden_state()
+    total_loss=0
+    for (batch,(inp,targ)) in enumerate(dataset.take(steps_per_epoch)):
+        batch_loss=train_step(inp,targ,enc_hidden)
+        total_loss+=batch_loss
+        if batch%20==0:
+            print("epoch {} Batch {} loss{}".format(epoch+1,batch,batch_loss.numpy()))
+    if (epoch+1)%5==0:
+        checkpoint.save(file_prefix=checkpoint_prefix)
+    print("Epoch {} loss{}".format(epoch+1,total_loss/steps_per_epoch))
+    print("time taken for 1 epoch{}".format(time.time()-start))
 
 # 定义测试和可视化函数
 def evaluate(sentence):
